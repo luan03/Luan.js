51a741db50cac0e358caeb0abdd04490d77acb45
Luan <luan03@msn.com>
2012-10-26 22:30:22 -0200

    organizacao

--- js/Luan.js
+++ js/Luan.js
@@ -1,92 +1,92 @@
 var Luan = Luan || {};
-            Luan.Utils = Luan.Utils || {};
-            Luan.Models = Luan.Models || {};
-            Luan.Views = Luan.Views || {};
+    Luan.Utils = Luan.Utils || {};
+    Luan.Models = Luan.Models || {};
+    Luan.Views = Luan.Views || {};
 
 
         // ----------------------------------------------------------------------- Main 
 
-        Luan.Utils = {
+Luan.Utils = {
 
-            
-            AppStart: function(){
-                Luan.Views.Logs(Luan.Views.messageViews.MSGinit);
+    
+    AppStart: function(){
+        Luan.Views.Logs(Luan.Views.messageViews.MSGinit);
 
-                //Init Singleton
-                Luan.Utils.singleton.init();
-                
-                var el = document.getElementById('elemento');
-                el.addEventListener("click", this.imprime, false);
-            },
-
-            imprime: function(){
-                console.log(Luan.Utils.config.CONSTANTS.STATEVIEW.DDINPROGRESS);
-            }
-        };
-
-
-        //Config Project
-        Luan.Utils.config = {
-            version: "1.0.1",
-            statusApp: "Loaded",
-            timeRun: "full time",
-            queued: false,
+        //Init Singleton
+        Luan.Utils.singleton.init();
         
-            CONSTANTS: {
-                INIT: { INITOK: 1, INITFAIL: 0},
-                STATEVIEW: { IDLE: "IDLE", DDINPROGRESS: "DDINPROGRESS"}
-            }
-            
-        };
+        var el = document.getElementById('elemento');
+        el.addEventListener("click", this.imprime, false);
+    },
 
-        //Create Object Pattern
-        Luan.Utils.Singleton = {};
+    imprime: function(){
+        console.log(Luan.Utils.config.CONSTANTS.STATEVIEW.DDINPROGRESS);
+    }
+};
+
+
+//Config Project
+Luan.Utils.config = {
+    version: "1.0.1",
+    statusApp: "Loaded",
+    timeRun: "full time",
+    queued: false,
+
+    CONSTANTS: {
+        INIT: { INITOK: 1, INITFAIL: 0},
+        STATEVIEW: { IDLE: "IDLE", DDINPROGRESS: "DDINPROGRESS"}
+    }
+    
+};
+
+//Create Object Pattern
+Luan.Utils.Singleton = {};
 
 
 
             // -------------------------------------------------------------------  Models
 
-             Luan.Models = {
-                CollectionItens: new Array(),
+Luan.Models = {
+    CollectionItens: new Array(),
 
-                setCollection: function(o){
-                   return this.CollectionItens.push(o);
-                }
-            };
+    setCollection: function(o){
+       return this.CollectionItens.push(o);
+    }
+};
 
-            Luan.Models.List = {
-                ListItens: [
-                        {Name : "Produto1", Price : 8.10, InStock : true },
-                        {Name : "Produto2", Price : 8.20, InStock : true },
-                        {Name : "Produto",  Price : 8.30, InStock : false},
-                        {Name : "Produto3", Price : 8.40, InStock : true },
-                        {Name : "Produto4", Price : 8.50, InStock : true }
-                    ],
+Luan.Models.List = {
+    ListItens: [
+            {Name : "Produto1", Price : 8.10, InStock : true },
+            {Name : "Produto2", Price : 8.20, InStock : true },
+            {Name : "Produto",  Price : 8.30, InStock : false},
+            {Name : "Produto3", Price : 8.40, InStock : true },
+            {Name : "Produto4", Price : 8.50, InStock : true }
+        ],
 
-                totalList: function() {
-                    Luan.Views.Logs(Luan.Views.messageViews.MSGLoadData);
-                    return Luan.Models.List.ListItens.length;
-                },
+    totalList: function() {
+        Luan.Views.Logs(Luan.Views.messageViews.MSGLoadData);
+        return Luan.Models.List.ListItens.length;
+    },
 
-                getItens: function(){
-                    consol.log(ListItens);
-                },
+    getItens: function(){
+        consol.log(ListItens);
+    },
 
-                setItens : Luan.Models.setCollection()
-            };
+    setItens : Luan.Models.setCollection()
+};
 
 
             //---------------------------------------------------------------------- Views
 
-            Luan.Views = {
-                messageViews: {
-                    MSGinit: 'Application initialized...',
-                    MSGFinish: 'Application Finishing...',
-                    MSGLoadData: 'Data Loaded',
-                    initSingleton: "Singleton initialized..."
-                },
+Luan.Views = {
+    messageViews: {
+        MSGinit: 'Application initialized...',
+        MSGFinish: 'Application Finishing...',
+        MSGLoadData: 'Data Loaded',
+        initSingleton: "Singleton initialized..."
+    },
 
-                Logs: function (msg){
-                    console.log(msg)
-                }
-            };
+    Logs: function (msg){
+        console.log(msg)
+    }
+};
---
+++ readme/README.md
@@ -0,0 +1,38 @@
+<h1>Luan.js</h1>
+
+<p>Luan.js dá estrutura para aplicações web, utilizando MVC, Design Patterns e OOP.</p>
+
+<h2>MVC</h2>
+<p>Foram criados os seguintes modulos:</p>
+
+<ul>
+ <li><code>Model</code> : manipulação dos dados</li>
+ <li><code>View</code> : mensagens, logs, displays e modal</li>
+ <li><code>Controller (Utils)</code> : Métodos de negócio</li>
+</ul>
+
+<p>Os Controllers podem ser extendidos a frameworks de terceiros.</p>
+
+<h2>Design Patterns</h2>
+
+<strong>Creational Pattern</strong>
+<ul>
+<li>Manipulação e criação de objetos</li>
+<li>Controle  da criação do mesmo</li>
+</ul>
+<p>Padrões:  Constructor, Factory, Abstract, <a href="javascript:void(0);">Prototype</a>, <a href="js/Singleton.js">Singleton</a> and <a href="javascript:void(0);">Builder</a>.</p>
+
+<strong>Structural Pattern</strong>
+<ul>
+<li>Estrutura do projeto</li>
+<li>Composição dos objetos para fazer  relação dos mesmos.</li>
+</ul>	
+<p>Padrões:  Decorator, <a href="javascript:void(0);">Composite</a>, <a href="javascript:void(0);">Facade</a>, Flyweight, Adapter and Proxy. </p>
+
+<strong>Behavioral Pattern</strong>
+<ul>
+<li>Simplificar comunicação entre objetos</li>
+</ul>
+<p>Padrões:  Iterator, <a href="javascript:void(0);">Mediator</a>, <a href="javascript:void(0);">Observer</a> and Visitor.</p>
+
+<a href="http://net.tutsplus.com/tutorials/javascript-ajax/digging-into-design-patterns-in-javascript/" target="_blank">Implements</a>
